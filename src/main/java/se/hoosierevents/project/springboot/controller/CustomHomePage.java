04a962cf9bccf1c71a11a563fbd22cafcf10d922
Harish M <harishmaruthi4@gmail.com>
2018-11-20 01:50:30 -0500

    Event Image server code

--- src/main/java/se/hoosierevents/project/springboot/controller/CustomHomePage.java
+++ src/main/java/se/hoosierevents/project/springboot/controller/CustomHomePage.java
@@ -5,14 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import javax.servlet.http.HttpSession;
-
 @RestController
 public class CustomHomePage extends WebMvcConfigurerAdapter {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/homepage").setViewName("homepage");
+        registry.addViewController("/").setViewName("homepage");
         registry.addViewController("/sign").setViewName("signup");
         registry.addViewController("/eventpage").setViewName("eventpage");
         registry.addViewController("/search").setViewName("Search");
--- src/main/java/se/hoosierevents/project/springboot/controller/EventController.java
+++ src/main/java/se/hoosierevents/project/springboot/controller/EventController.java
@@ -5,13 +5,18 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -47,16 +52,24 @@ public class EventController implements Controller {
 		return null;
 	}
 
-	@RequestMapping("/getImage")
-	public ResponseEntity getEventImage(@RequestParam("img") String imagePath) {
+	@RequestMapping(value = "/image-byte-array", method = RequestMethod.GET)
+	public @ResponseBody byte[] getImageAsByteArray() throws IOException {
+		String imagePath = "theatre2.jpg";
 		Resource file = fileStoreService.loadAsResource(imagePath);
-		try {
-			return ResponseEntity.ok(file.getURI());
+	    return IOUtils.toByteArray(file.getInputStream());
+	}
+	
+	@RequestMapping(value = "/getImage")
+	@ResponseBody
+	public byte[] getEventImage(@RequestParam("img") String imagePath) {
+		Resource file = fileStoreService.loadAsResource(imagePath);
+		 try {
+			return IOUtils.toByteArray(file.getInputStream());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return ResponseEntity.ok(new Exception());
+		return null;
 	}
 
 	@RequestMapping("/getEvent")
@@ -71,7 +84,7 @@ public class EventController implements Controller {
 		eventService.saveEvent(event, file);
 		redirectAttributes.addFlashAttribute("message",
 				"You successfully uploaded " + file.getOriginalFilename() + "!");
-		return new RedirectView("eventPage.html");
+		return new RedirectView("eventpage");
 	}
 
 	@RequestMapping("/knowMore")
