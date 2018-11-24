92e35749a8f0787ed756899f4b10972d27fe8293
Harish M <harishmaruthi4@gmail.com>
2018-11-24 14:42:08 -0500

    Update User.java

--- src/main/java/se/hoosierevents/project/model/User.java
+++ src/main/java/se/hoosierevents/project/model/User.java
@@ -1,13 +1,25 @@
 package se.hoosierevents.project.model;
 
-import org.hibernate.validator.constraints.NotEmpty;
+import java.io.Serializable;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name = "user_details")
-public class User {
+public class User implements  Serializable{
 
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Column(name = "user_id", unique=true)
 	@GeneratedValue(strategy = GenerationType.AUTO)
