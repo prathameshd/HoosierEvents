f902a16df168308b86700f9fe19775a5dbc78664
Vanessa Pinto <vanessapinto257@gmail.com>
2018-11-30 17:23:54 -0500

    Update Search.js

--- src/main/resources/static/script/Search.js
+++ src/main/resources/static/script/Search.js
@@ -1,14 +1,1 @@
 
-function DisplayAllEvents(){
-	console.log("Hello");
-	$.ajax({
-		type:'POST',
-		dataType:'JSON',
-		url:"/getAllEvents",
-		  success: function(data) {
-				console.log('success',data);
-				document.getElementById("event_title").innerHTML=data;
-
-		   },
-		   error:function(exception){alert('Exception:'+exception);}})
-}
