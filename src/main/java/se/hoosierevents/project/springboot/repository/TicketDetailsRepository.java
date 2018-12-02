8e7086394a65c68327303bf3358eb1836f885fce
Vanessa Pinto <vanessapinto257@gmail.com>
2018-12-02 18:55:04 -0500

    Update TicketDetailsRepository.java

--- src/main/java/se/hoosierevents/project/springboot/repository/TicketDetailsRepository.java
+++ src/main/java/se/hoosierevents/project/springboot/repository/TicketDetailsRepository.java
@@ -1,5 +1,6 @@
 package se.hoosierevents.project.springboot.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,4 +16,9 @@ public interface TicketDetailsRepository extends CrudRepository<TicketDetails, L
 	final Long TICKET_TYPE_GOLD_ID = 1L;
 	//@Query("SELECT t FROM TicketDetails t where t.event_id = ?1 AND t.ticket_type = ?2")
     //public TicketDetails findByEventAndType(Long event_id, Long ticket_type);
+	
+	final String EVENT_FIND_PRICE = "select price from ticket_breakdown where event_id=?1 AND ticket_type=?2";
+	
+	@Query(value = EVENT_FIND_PRICE, nativeQuery = true)
+	Float findTicketPrice(Long event_id, int ticket_type);
 }
